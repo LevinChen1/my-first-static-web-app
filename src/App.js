96bdf0577fc9d627dc7c157d204ae6172baec8c4
Levin Chen <lxc@sydgram.nsw.edu.au>
2025-06-25 13:00:20 +1000

    login test

--- src/App.js
+++ src/App.js
@@ -2,32 +2,34 @@ import React from 'react';
 
 function App() {
   const value = 'HH';
+  var User = {}
+  async function getUserInfo() {
+    const response = await fetch('/.auth/me');
+    const payload = await response.json();
+    const { clientPrincipal } = payload;
+    return clientPrincipal;
+  }
 
-  // async function getUserInfo() {
-  //   const response = await fetch('/.auth/me');
-  //   const payload = await response.json();
-  //   const { clientPrincipal } = payload;
-  //   return clientPrincipal;
-  // }
-
-  // (async () => {
-  // console.log(await getUserInfo());
-  // })();
-  // var User = {}
-  fetch('/.auth/me')
-    .then(response => {
-      if (!response.ok) {
-        throw new Error(`HTTP error! Status: ${response.status}`);
-      }
-      return response.json(); // Parse the JSON from the response
-    })
-    .then(data => {
-      User = data
-      console.log('Data received:', data); // Handle the data
-    })
-    .catch(error => {
-      console.error('Error occurred:', error); // Handle errors
-    });
+  (async () => {
+  User = await getUserInfo()
+  console.log(User);
+  console.log(User.userDetails);
+  })();
+  
+  // fetch('/.auth/me')
+  //   .then(response => {
+  //     if (!response.ok) {
+  //       throw new Error(`HTTP error! Status: ${response.status}`);
+  //     }
+  //     return response.json(); // Parse the JSON from the response
+  //   })
+  //   .then(data => {
+  //     User = data
+  //     console.log('Data received:', data); // Handle the data
+  //   })
+  //   .catch(error => {
+  //     console.error('Error occurred:', error); // Handle errors
+  //   });
 
   return (<div>
   <div>Hello</div>
