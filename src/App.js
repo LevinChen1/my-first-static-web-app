8677db71976e6269d799f1026f0400b49cc3ab65
Levin Chen <lxc@sydgram.nsw.edu.au>
2025-06-25 12:44:08 +1000

    print username

--- src/App.js
+++ src/App.js
@@ -3,19 +3,35 @@ import React from 'react';
 function App() {
   const value = 'HH';
 
-  async function getUserInfo() {
-    const response = await fetch('/.auth/me');
-    const payload = await response.json();
-    const { clientPrincipal } = payload;
-    console.log(clientPrincipal)
-    return clientPrincipal;
-  }
+  // async function getUserInfo() {
+  //   const response = await fetch('/.auth/me');
+  //   const payload = await response.json();
+  //   const { clientPrincipal } = payload;
+  //   return clientPrincipal;
+  // }
 
+  // (async () => {
+  // console.log(await getUserInfo());
+  // })();
+  var User = {}
+  fetch('/.auth/me')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+      return response.json(); // Parse the JSON from the response
+    })
+    .then(data => {
+      User = data
+      console.log('Data received:', data); // Handle the data
+    })
+    .catch(error => {
+      console.error('Error occurred:', error); // Handle errors
+    });
 
   return (<div>
-  <div>Hello {value}</div>
+  <div>Hello {User.userDetails}</div>
   <a href="/.auth/login/aad">Microsoft login</a> 
-  <button onClick={getUserInfo()}></button>
   </div>
   )
   ;
