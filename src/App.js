33c21d8df86212735112a7782638cc0f1416858e
Levin Chen <lxc@sydgram.nsw.edu.au>
2025-06-25 12:00:39 +1000

    check user details

--- src/App.js
+++ src/App.js
@@ -2,6 +2,19 @@ import React from 'react';
 
 function App() {
   const value = 'HH';
+
+  async function getUserInfo() {
+    const response = await fetch('/.auth/me');
+    const payload = await response.json();
+    const { clientPrincipal } = payload;
+    return clientPrincipal;
+  }
+
+  (async () => {
+  console.log(await getUserInfo());
+  })();
+
+
   return (<div>
   <div>Hello {value}</div>
   <a href="/.auth/login/aad">Microsoft login</a> 
